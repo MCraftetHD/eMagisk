ac67d3e49dd1e2379c8a6f6525b9f761c818cc65
MCraftetHD <72506563+MCraftetHD@users.noreply.github.com>
2024-10-22 13:10:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -218,13 +218,8 @@ on_install() {
     ui_print " "
     ui_print " "
     ui_print "================================================"
-    ui_print " Do you want to install ATV services?"
-    ui_print "   Press VOLUME UP to SKIP INSTALLATION."
-    ui_print "   Press VOLUME DOWN to INSTALL ATV Services."
-    ui_print " "
-    ui_print "   After 10 seconds services will be installed!"
-    ui_print " "
-    timeout 10 /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME >$TMPDIR/events
+    ui_print "installing ATV services..."
+    /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME >$TMPDIR/events
 
     ui_print " "
 
